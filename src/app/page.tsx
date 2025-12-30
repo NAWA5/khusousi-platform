b049bfa89fc4512de46f6cf100a21ff8566d506e
NAWA5 <nawaf773323@gmail.com>
2025-12-30 04:14:09 +0300

    Add shroo7-style categories and CTA/footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,21 @@ import PlatformPillars from "@/components/home/PlatformPillars";
 import Universities from "@/components/home/Universities";
 import FeaturedCourses from "@/components/home/FeaturedCourses";
 import MobileAppCTA from "@/components/home/MobileAppCTA";
+import TopCategories from "@/components/home/TopCategories";
+import JoinCTA from "@/components/home/JoinCTA";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Hero />
       <Categories />
+      <TopCategories />
       <Universities />
       <FeaturedCourses />
       <PlatformPillars />
       <LatestCourses />
       <MobileAppCTA />
+      <JoinCTA />
       <Features />
     </div>
   );
